f49e6a9ea67140cec4e9ab8987c5aa4c462aacb0
OtaraAlex <mbechealex61@gmail.com>
2024-02-06 13:55:42 +0300

    Complete signin page UI and functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -48,7 +48,7 @@ export default function SignUp() {
         {/* left */}
         <div className="flex-1">
           <Link to="/" className="text-4xl font-bold dark:text-white">
-            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg">
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
               Alex's</span>Blog
           </Link>
           <p className="text-sm mt-5">
