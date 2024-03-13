18fbd7ec0ffa33209616f4929084d5c0d1fb0dfa
OtaraAlex <mbechealex61@gmail.com>
2024-03-13 19:46:16 +0300

    Refactor Home page layout and add Button component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
+import { Button } from "flowbite-react";
 import CallToAction from "../components/CallToAction";
-import { useEffect, useState } from "react";
 import PostCard from "../components/PostCard";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const [posts, setPosts] = useState([]);
@@ -15,41 +16,47 @@ export default function Home() {
     fetchPosts();
   }, []);
   return (
-    <div>
-      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto ">
-        <h1 className="text-3xl font-bold lg:text-6xl">Welcome to my Blog</h1>
-        <p className="text-gray-500 text-xs sm:text-sm">
-          A place where I share my thoughts and experiences
-        </p>
-        <Link
-          to="/search"
-          className="text-xs sm:text-sm text-teal-500 font-bold hover:underline"
-        >
-          View all posts
-        </Link>
-      </div>
-      <div className="p-3 bg-amber-100 dark:bg-slate-700">
-        <CallToAction />
-      </div>
+    <>
+      <section className="py-5 md:py-10">
+        <div className="max-w-7xl lg:mx-auto p-5 md:px-10 xl:px-0 w-full grid grid-cols-1 gap-5 md:grid-cols-2">
+          <div className="flex flex-col justify-center gap-8">
+            <h1 className="font-bold text-[40px] leading-[48px] lg:text-[48px] lg:leading-[60px]  xl:text-[58px] xl:leading-[74px]">
+              Welcome to my Blog!
+            </h1>
+            <p className="text-[20px] font-normal leading-[30px] tracking-[2%] md:font-normal md:text-[24px] md:leading-[36px]">
+              This is a blog where I write about my thoughts, ideas, and things
+              I find interesting. I hope you enjoy reading my posts.
+            </p>
+            <Button pill>
+              <Link
+                to="/search"
+                className="text-xs sm:text-sm text-white font-bold hover:underline"
+              >
+                View all posts
+              </Link>
+            </Button>
+          </div>
 
-      <div className="max-w-6xl mx-auto p-3 flex flex-col py-7">
-        {posts && posts.length > 0 && (
-          <div className="flex flex-col gap-6">
-            <h2 className="text-2xl font-semibold text-center">Recent Posts</h2>
-            <div className="flex flex-wrap gap-4">
+          <div className="p-3 bg-amber-100 dark:bg-slate-700">
+            <CallToAction />
+          </div>
+        </div>
+      </section>
+      
+      {posts && posts.length > 0 && (
+        <section className="max-w-7xl lg:mx-auto p-5 md:px-10 xl:px-0 w-full my-8 flex flex-col gap-8 md:gap-12">
+          <h2 className="text-center font-bold text-[32px] leading-[40px] lg:text-[36px] lg:leading-[44px] xl:text-[40px] xl:leading-[48px]">
+            Recent Posts
+          </h2>
+          <div className="flex flex-col items-center">
+            <div className="grid w-full grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:gap-10">
               {posts.map((post) => (
                 <PostCard key={post._id} post={post} />
               ))}
             </div>
-            <Link
-              to={"/search"}
-              className="text-lg text-teal-500 hover:underline text-center"
-            >
-              View all posts
-            </Link>
           </div>
-        )}
-      </div>
-    </div>
+        </section>
+      )}
+    </>
   );
 }
