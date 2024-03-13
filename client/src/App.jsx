d14436832cb02c3b6aebe6cba1a8a52a8224f4c6
OtaraAlex <mbechealex61@gmail.com>
2024-03-13 19:17:40 +0300

    Fix capitalization of About component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/about";
+import About from "./pages/About";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import Dashboard from "./pages/Dashboard";
